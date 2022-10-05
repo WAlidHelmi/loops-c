460d09032efacd7ced77c8def6f39349fe2a777a
WAlidHelmi <113910191+WAlidHelmi@users.noreply.github.com>
2022-10-05 16:24:11 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 100; index++) {
+        number += 1
+        basic.showNumber(number)
+    }
+})
+let number = 0
+number = 0
+basic.showNumber(number)
